b861a2dc9f1479dd2f888c6954480005f1116123
unknown <saritapra931@gmail.com>
2024-01-02 22:03:10 +0530

    build folder add

---
+++ build/precache-manifest.f1a6a93616af5b6c596a06e3cc4d7cf6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3762c5550e848884966",
+    "url": "/static/css/main.5eda8469.chunk.css"
+  },
+  {
+    "revision": "d3762c5550e848884966",
+    "url": "/static/js/main.d3762c55.chunk.js"
+  },
+  {
+    "revision": "8a5b0b70b12187e5da28",
+    "url": "/static/js/1.8a5b0b70.chunk.js"
+  },
+  {
+    "revision": "6d0168dd101359e66ebb",
+    "url": "/static/js/2.6d0168dd.chunk.js"
+  },
+  {
+    "revision": "a46ef3f558368343807d",
+    "url": "/static/js/runtime~main.a46ef3f5.js"
+  },
+  {
+    "revision": "864732a40cf60ddbf25d82359e216724",
+    "url": "/static/media/loginpic.864732a4.png"
+  },
+  {
+    "revision": "12e12bea6d825b4eaba148dba171326e",
+    "url": "/static/media/signup.12e12bea.png"
+  },
+  {
+    "revision": "d801283e6ef270baa9cae366396d9bf9",
+    "url": "/index.html"
+  }
+];
